eeb07cb3881853ca0a89722dcefd1f910d14ac5a
Анастасия Мединова <medinova.a@web-industry.pro>
2019-03-22 12:10:45 +0300

    заливка файлов

--- js/script.js
+++ js/script.js
@@ -1,56 +1,27 @@
 $(document).ready(function () {
-    $('.js_reg-btn').on('click', function () {
-        $('.js-modal-reg-block').slideDown();
-    });
-    $('.js_header-show-menu span').on('click', function () {
-        $('.js_header-menu-mobile').toggleClass('show');
+    $('.js_btn-accordion').on('click', function() {
+            $(this).parent('h5').toggleClass('active');
     });
 
-    $('.js_header-menu-nav a').on('mouseenter', function () {
-        $(this).parent().addClass('selected');
-    });
-    $('.js_header-menu-nav a').on('mouseleave', function () {
-        $(this).parent().removeClass('selected');
+    $('.js_block-header-list').on('click', function() {
+        $(this).find('ul').toggleClass('active');
     });
 
+    $('.js_block-header-menu').on('click', function() {
+        $(this).find('.js_header-menu-mobile').toggleClass('active');
+    });
 
+    $(function(){
+        $('a[href^="#"]').on('click', function(event) {
+            event.preventDefault();
 
-    function focusFunc() {
-        let con = 0;
-        $('.js_head-search-show').on('mouseenter', function () {
-            $(this).find('.js_head-search-block').addClass('show')
+            var sc = $(this).attr("href"),
+                dn = $(sc).offset().top - 25;
+
+            $('html, body').animate({scrollTop: dn}, 1000);
         });
-        $('.js_head-search-block input').on('focusin', function (e) {
-            if ($(this).is(e.target)) {
-                $(this).parent('.js_head-search-block').addClass('show');
-                con = 1;
-            }
-        });
-        $('.js_head-search-block input').on('focusout', function (e) {
-            if ($(this).is(e.target)) {
-                $(this).parent('.js_head-search-block').removeClass('show');
-                con = 0;
-            }
-        });
-
-
-        $('.js_head-search-show').on('mouseleave', function () {
-            if (con === 0) {
-                $(this).find('.js_head-search-block').removeClass('show');
-                con = 0;
-            } else {
-                $(this).find('.js_head-search-block').addClass('show');
-            }
-        });
-    }
-    $(window).on('resize', function(){
-        if ($(window).width() < 768) {
-            focusFunc()
-        }
     });
-    if ($(window).width() < 768) {
-        focusFunc()
-    }
+
 
 });
 
